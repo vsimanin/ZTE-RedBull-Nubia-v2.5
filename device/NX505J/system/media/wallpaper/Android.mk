cc423959cd0be20ba573244d7a2ab4780743aba5
Василий Симанин (LiveMoto) <vsimanin@gmail.com>
2014-11-30 18:13:15 +0300

    Лишнее!

--- device/NX505J/system/media/wallpaper/Android.mk
+++
@@ -1,8 +0,0 @@
-
-################################  common copy ##########################################################
-
-#thememanager
-$(shell mkdir -p --mode=755 $(TARGET_OUT)/media/wallpaper;)
-$(shell cp -a ./zte_vendor/IdleCommonResources/wallpaper/* $(TARGET_OUT)/media/wallpaper/.;)
-
-################################## common copy end ####################################################
